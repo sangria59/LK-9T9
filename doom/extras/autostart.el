f5b0841f2808122c85e371e5baaaf2d2cdf1db4f
Kernel Mustard <r00trotlog@proton.me>
2026-01-04 00:40:24 +0000

    Create autostart.el

---
+++ doom/extras/autostart.el
@@ -0,0 +1,9 @@
+;;; autostart.el -*- lexical-binding: t; -*-
+;;(require 'vterm)
+;;
+;;(add-hook 'emacs-startup-hook
+;;          (lambda ()
+;;            (let ((current-window (selected-window)))
+;;              (vterm)
+;;              (select-window current-window)))
+;;          t)
